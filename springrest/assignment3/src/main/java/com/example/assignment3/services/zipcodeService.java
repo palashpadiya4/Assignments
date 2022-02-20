31c9a5b7ec13692eabdf6353349e9c7e7ac74edb
palashpadiya4 <97250940+palashpadiya4@users.noreply.github.com>
2022-02-20 14:20:01 +0530

    Add files via upload

---
+++ springrest/assignment3/src/main/java/com/example/assignment3/services/zipcodeService.java
@@ -0,0 +1,28 @@
+package com.example.assignment3.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.assignment3.entities.zipcode;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class zipcodeService {
+
+    private static List<zipcode> list = new ArrayList<zipcode>();
+
+    static {
+        list.add(new zipcode(99501, "AK", "ANCHORAGE", "US"));
+    }
+
+    public zipcode getzipcodeByzipcode(int zipcode) {
+
+        return list.stream().filter(z -> z.getZipcode() == zipcode).findFirst().get();
+    }
+
+    public List<zipcode> getAll() {
+        return list;
+    }
+
+}
